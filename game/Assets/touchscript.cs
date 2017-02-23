a9f0cbc47abcd136cb6e6b2930365d15f22e037a
ReesRingrose <c15388176@mydit.ie>
2017-02-23 15:05:22 +0000

    touch test

--- game/Assets/touchscript.cs
+++ game/Assets/touchscript.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class touchscript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+
+    public float speed = 0.1F;
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+        }
+    }
 }
