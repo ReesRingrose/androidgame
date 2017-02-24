9836453c68496165ba747984bdcd9e743ff3cc2f
ReesRingrose <c15388176@mydit.ie>
2017-02-24 17:48:43 +0000

    new
    update game

--- game/Assets/touchscript.cs
+++ game/Assets/touchscript.cs
@@ -6,16 +6,12 @@ public class touchscript : MonoBehaviour {
 
     // Use this for initialization
 
-    public float speed = 0.1F;
+    void start()
+    {
+        Debug.Log("hello world yes sir ");
+    }
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            // Get movement of the finger since last frame
-            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-
-            // Move object across XY plane
-            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
-        }
+        
     }
 }
