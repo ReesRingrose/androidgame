ba70c58c964a1da07c5dd9f72528719bfbc84347
ReesRingrose <c15388176@mydit.ie>
2017-03-01 21:13:50 +0000

    load screen

---
+++ game/New Unity Project/Assets/scripts/preloader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class preloader : MonoBehaviour {
+
+    private CanvasGroup fadeGroup;
+    private float loadTime;
+    private float minimunLogoTime = 3.0f; //minimun time in the scene
+
+   private void start()
+    {
+        //grab the canvas group
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        fadeGroup.alpha = 1;
+
+        if (Time.time < minimunLogoTime)
+            loadTime = minimunLogoTime;
+        else
+            loadTime = Time.time;
+    }
+
+    private void update()
+    {
+        if (Time.time < minimunLogoTime)
+        {
+            fadeGroup.alpha = 1 - Time.time;
+        }
+         
+        if(Time.time > minimunLogoTime && loadTime != 0 )
+            {
+            fadeGroup.alpha = Time.time - minimunLogoTime;
+            if (fadeGroup.alpha >=1)
+            {
+                Debug.Log("Change the scene");
+            }
+
+        }
+    }
+}
